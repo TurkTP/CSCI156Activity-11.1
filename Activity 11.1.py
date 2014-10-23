a8ecfcb1b6c7747227863b32a68ddeb6c25f10a9
TurkTP <lej1@alfred.edu>
2014-10-23 16:40:51 -0400

    Github.

---
+++ Activity 11.1.py
@@ -0,0 +1,12 @@
+__author__ = 'Luke'
+RSkills={['Str']:['Dragon Battleaxe'],['Att']:'Dragon Longsword'],['Def']:['Dragon Battleaxe'],['Hit']:'Dragon Longsword']}
+
+print(RSkills['Att'])
+print(Rskills.get('Def'))
+for skill in RSkills:
+    print(skill)
+for skill in Rskills:
+    if skill == ("pie")
+        print("I like pie.")
+    else:
+        print("Pie is not a car!")
